b15f876235722e182cfebec3abf402f7d4a3537e
lisacharuma <charumalisah@gmail.com>
2024-07-19 10:30:51 +0000

    Basic authentication

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -25,38 +25,44 @@ elif AUTH_TYPE == "basic_auth":
 
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """ 404 handler
+    """ Not found handler
     """
     return jsonify({"error": "Not found"}), 404
 
 
 @app.errorhandler(401)
 def unauthorized_error(error) -> str:
-    """ Unauthorized error handler
+    """ Unauthorized handler
     """
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
 def forbidden_error(error) -> str:
-    """ Forbidden error handler
+    """ Forbidden handler
     """
     return jsonify({"error": "Forbidden"}), 403
 
 
 @app.before_request
 def before_request() -> str:
-    """ Before Request Validation Handler
+    """ Before Request Handler
+
+    Requests Validation
     """
     if auth is None:
         return
+
     excluded_paths = ['/api/v1/status/',
-                    '/api/v1/unauthorized/',
-                    '/api/v1/forbidden/']
+                      '/api/v1/unauthorized/',
+                      '/api/v1/forbidden/']
+
     if not auth.require_auth(request.path, excluded_paths):
         return
+
     if auth.authorization_header(request) is None:
         abort(401)
+
     if auth.current_user(request) is None:
         abort(403)
 
